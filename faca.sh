8977ee885e89b2032e2221836186c0dc6ddcc549
Ben Johnson <benjohnson2001@gmail.com>
2013-01-25 16:20:40 -0500

    succesfully extracted links for actor imdb pages from full cast html source

--- faca.sh
+++ faca.sh
@@ -18,15 +18,56 @@ fi
 # modify url to display full cast information
 url=$(grep Location imdb_messages.txt | cut -c 11- | rev | cut -c 13- | rev)fullcredits#cast
 
+
+
+# fetch text version of website
+lynx -dump "$url" > full_cast_and_crew.txt
+
+# find line numbers surrounding cast list
+start=$(grep -n "Cast (in.*order)" full_cast_and_crew.txt | cut -f1 -d:)
+end=$(grep -n "Produced by" full_cast_and_crew.txt | cut -f1 -d:)
+
+# print results if there is no cast (i.e. documentaries)
+if [[ $start -eq 0 || $end -eq 0 ]]; then
+
+	# clear the last three lines
+	#	 \r 		moves cursor to beginning of the line
+	#	 \033[1A	moves cursor up one line		
+	#	 \033[K 	clears the line for new text		
+	#	 -n 		does not output the trailing newline
+	#	 -e 		enables escape sequences	
+	echo -e -n "\r\033[K\033[1A\r\033[K\033[1A\r\033[K\033[1A\r\033[K"
+
+	echo -e "\r\033[KMovie: $1\r\t\t\t\t\t    Average Age: no ages listed"
+	IFS=$OLD_IFS
+	rm imdb_messages.txt
+	rm full_cast_and_crew.txt
+	exit 0
+fi
+
+# crop document for cast list
+sed -n "$start,${end}p" full_cast_and_crew.txt > full_cast.txt
+
+# remove left over junk between square brackets
+sed -i -n '1h;1!H;${;g;s/\[[^][]*\]//g;p;}' full_cast.txt
+
+# remove more cruft by delimiting lines with ... using grep and awk and removing leading white space with sed
+rawlist=$(cat full_cast.txt | grep '\.\.\.' | awk 'BEGIN {FS = "[.][.][.]"} ; NF {print $1}' | sed -e 's/^[ \t]*//')
+
+
+
 		# save html source to file
 		wget -U firefox "$url" 2> /dev/null
 		
 		# find line numbers surrounding cast list
-		start=$(grep -n "(in.*order)" fullcredits | cut -f1 -d:)
+		start=$(grep -n "Cast.*(in.*order)" fullcredits | cut -f1 -d:)
 		end=$(grep -n "Produced by" fullcredits | cut -f1 -d:)		
 		
+#		echo $start
+#		echo $end
+		
 		# crop document for cast list
-		sed -n "$start,$(($end-1))p" fullcredits | awk 'BEGIN {FS = "rder)" } ; NF {print $2}' > castlist.txt
+		sed -n "$start,$(($end-1))p" fullcredits | awk 'BEGIN {FS = "Cast.*(in.*order).*<small>" } ; NF {print $2}' > castlist.txt
 				
 		rm fullcredits		
 				
@@ -46,43 +87,16 @@ url=$(grep Location imdb_messages.txt | cut -c 11- | rev | cut -c 13- | rev)full
 			((i++))
 		done		
 
-# fetch text version of website
-lynx -dump "$url" > full_cast_and_crew.txt
-
-# find line numbers surrounding cast list
-start=$(grep -n "Cast (in.*order)" full_cast_and_crew.txt | cut -f1 -d:)
-end=$(grep -n "Produced by" full_cast_and_crew.txt | cut -f1 -d:)
-
-# print results if there is no cast (i.e. documentaries)
-if [[ $start -eq 0 ]]; then
-
-	# clear the last three lines
-	#	 \r 		moves cursor to beginning of the line
-	#	 \033[1A	moves cursor up one line		
-	#	 \033[K 	clears the line for new text		
-	#	 -n 		does not output the trailing newline
-	#	 -e 		enables escape sequences	
-	echo -e -n "\r\033[K\033[1A\r\033[K\033[1A\r\033[K\033[1A\r\033[K"
-
-	echo -e "\r\033[KMovie: $1\r\t\t\t\t\t    Average Age: no ages listed"
-	IFS=$OLD_IFS
-	rm imdb_messages.txt
-	rm full_cast_and_crew.txt
-	exit 0
-fi
 
 
-# crop document for cast list
-sed -n "$start,${end}p" full_cast_and_crew.txt > full_cast.txt
 
-# remove left over junk between square brackets
-sed -i -n '1h;1!H;${;g;s/\[[^][]*\]//g;p;}' full_cast.txt
 
-# remove more cruft by delimiting lines with ... using grep and awk and removing leading white space with sed
-rawlist=$(cat full_cast.txt | grep '\.\.\.' | awk 'BEGIN {FS = "[.][.][.]"} ; NF {print $1}' | sed -e 's/^[ \t]*//')
 
 #accumulator
 acc=0
+
+# number of actors
+nac=0
 
 # store actor names in bash array
 iter=0
@@ -90,27 +104,17 @@ for t in ${rawlist[@]}
 do
 
 	actors[$iter]="$t"
-
+		
 	# retrieve html source file of actor's imdb page
 	wget -O ${actors[$iter]} -U firefox "${actor_url[$iter]}" 2> imdb_messages.txt
 					
-	# error handling if there is connection problem
-	if ! grep -q "saved" imdb_messages.txt; then
-		echo "[ERROR] could not connect to imdb.com"
-		rm imdb_messages.txt
-		rm full_cast_and_crew.txt
-		rm full_cast.txt
-		rm ${actors[$iter]}		
-		IFS=$OLD_IFS		
-		exit 1
-	fi
-
 	# if birth date is not provided, continue to the next actor
 	if ! grep -q "birthDate\" datetime=" *"${actors[$iter]}"*; then
 		echo -e -n "\r\033[K\033[1A\r\033[K\033[1A\r\033[K\033[1A\r\033[K"
 		echo -n -e "\r\033[KMovie: $1\n  |\n  | Actor: ${actors[$iter]}\n  | Age: not listed"
 				
-		((iter++))	
+		rm ${actors[$iter]}					
+		((iter++))
 		continue
 	fi		
 
@@ -143,7 +147,9 @@ do
 
 	# clear files for next loop
 	rm imdb_messages.txt
+	rm ${actors[$iter]}	
 
+	((nac++))
 	((iter++))	
 done
 
@@ -158,7 +164,7 @@ done
 	fi
 
 	# calculate average age of cast
-	average_age=$(bc <<< "scale = 2; $acc / $iter")
+	average_age=$(bc <<< "scale = 2; $acc / $nac")
 
 	# clear last three lines and print result
 	echo -e -n "\r\033[K\033[1A\r\033[K\033[1A\r\033[K\033[1A\r\033[K"
@@ -174,7 +180,11 @@ fi
 
 rm full_cast_and_crew.txt
 rm full_cast.txt
-rm ${actors[$iter]}	
+
+# check if file exists using wildcard
+if ls search* > /dev/null 2>&1; then
+	rm search*
+fi
 
 # return environment variable to previous state
 IFS=$OLD_IFS
